0ac56c834b851e570b34dc00d691b8c57a2aa211
Abdo <abdouumostafa1@gmail.com>
2023-06-24 11:55:24 +0300

    initial commit

---
+++ src/components/AddToCart.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
+import { useCartContext } from "../context/cart_context";
+import AmountButtons from "./AmountButtons";
+
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product;
+
+  const [mainColor, setMainColor] = useState(colors[0]);
+
+  const [amount, setAmount] = useState(1);
+
+  const { addToCart } = useCartContext();
+  // console.log(stock);
+  const increase = () => {
+    if (stock !== amount) {
+      return setAmount(amount + 1);
+    }
+    return setAmount(stock);
+  };
+
+  const decrease = () => {
+    if (amount !== 1) {
+      return setAmount(amount - 1);
+    }
+    setAmount(1);
+  };
+
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span>colors : </span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                style={{ backgroundColor: color }}
+                key={index}
+                onClick={() => setMainColor(color)}
+                className={
+                  mainColor === color ? "color-btn active" : "color-btn"
+                }
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+      </div>
+      <Link
+        to="/cart"
+        className="btn"
+        onClick={() => addToCart(id, mainColor, amount, product)}
+      >
+        add to cart
+      </Link>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  margin-top: 2rem;
+  .colors {
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    align-items: center;
+    margin-bottom: 1rem;
+    span {
+      text-transform: capitalize;
+      font-weight: 700;
+    }
+    div {
+      display: flex;
+    }
+  }
+  .color-btn {
+    display: inline-block;
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+    background: #222;
+    margin-right: 0.5rem;
+    border: none;
+    cursor: pointer;
+    opacity: 0.5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      font-size: 0.75rem;
+      color: var(--clr-white);
+    }
+  }
+  .active {
+    opacity: 1;
+  }
+  .btn-container {
+    margin-top: 2rem;
+  }
+
+  .btn {
+    margin-top: 1rem;
+    width: 140px;
+  }
+`;
+export default AddToCart;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Private from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export { Home, About, Cart, Checkout, Error, Private, Products, SingleProduct };
